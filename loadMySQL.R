1862476ae0d33d74265eea9192b8ca4984a268ff
Regina Gurung <reginagurung@yahoo.co.uk>
2017-12-30 01:15:30 +0000

    Load storm data from dplyr library to MySQL.

---
+++ loadMySQL.R
@@ -0,0 +1,20 @@
+library(dplyr)
+
+library(RMySQL)
+
+my_storms = storms
+my_storms$DateTime = paste0(storms$year, "-",storms$month, "-",storms$day, " ", storms$hour,":00:00")
+my_storms = my_storms %>% select(name, DateTime, lat, long, status, category, wind, pressure)
+
+mydb = dbConnect(MySQL(), 
+                 dbname = "storms",
+                 host = "127.0.0.1",
+                 port = 3306,
+                 user = "root",
+                 password = "fantakim1234")
+
+
+dbWriteTable(mydb,'storms', 
+             my_storms, 
+             row.names = FALSE,
+             overwrite = TRUE)
